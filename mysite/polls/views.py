5fb4f551e18579d1b8288ebb74797b861a00f71a
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2016-01-11 19:54:28 +0800

    Add test and static file process

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,7 +6,7 @@ from .models import Question,Choice
 from django.core.urlresolvers import reverse
 from django.template import loader
 from django.views import generic
-
+from django.utils import timezone
 
 
 
@@ -15,7 +15,7 @@ class IndexView(generic.ListView):
 	template_name='polls/index.html'
 	context_object_name='latest_question_list'
 	def get_queryset(self):
-		return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
@@ -37,4 +37,3 @@ def vote(request,question_id):
 		selected_choice.votes+=1
 		selected_choice.save()
 		return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
-
