b3fed9d93ae657a4334afae89c50682d9b9d26e0
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2016-01-11 18:15:08 +0800

    add form process

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,30 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
-from .models import Question
-
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Question,Choice
+from django.core.urlresolvers import reverse
 from django.template import loader
+from django.views import generic
 
-def index(request):
-	latest_question_list=Question.objects.order_by('-pub_date')[:5]
-	# output=','.join([q.question_text for q in latest_question_list])
-	template=loader.get_template('polls/index.html')
-	context={
-	'latest_question_list':latest_question_list
-	}
-	return render(request,'polls/index.html',context)
 
-def results(request,question_id):
-	response='Your are looking at the results of question %s'
-	return HttpResponse(response%question_id)
+
+
+class IndexView(generic.ListView):
+	"""docstring for IndexView"""
+	template_name='polls/index.html'
+	context_object_name='latest_question_list'
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 
 def vote(request,question_id):
-	return HttpResponse('Your are voting on quesiton %s.'%question_id)
+	question=get_object_or_404(Question,pk=question_id)
+	try:
+		selected_choice=question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {'question': question,'error_message': "You didn't select a choice."})
+	else:
+		selected_choice.votes+=1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
