c7872b7734642a01e0ce5514584be9c7ff7c4eae
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2016-01-11 16:43:02 +0800

    add template support and link using namespace

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 from . import views
-print('include polls-url')
+# print('include polls-url')
+app_name = 'polls'
+
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
+
+from django.template import loader
 
 def index(request):
-	return HttpResponse('hello world  : mysite/polls app index.')
+	latest_question_list=Question.objects.order_by('-pub_date')[:5]
+	# output=','.join([q.question_text for q in latest_question_list])
+	template=loader.get_template('polls/index.html')
+	context={
+	'latest_question_list':latest_question_list
+	}
+	return render(request,'polls/index.html',context)
+
+def results(request,question_id):
+	response='Your are looking at the results of question %s'
+	return HttpResponse(response%question_id)
+
+def vote(request,question_id):
+	return HttpResponse('Your are voting on quesiton %s.'%question_id)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
