5284f7bc25d0d202f59ef4dbb36cd9a9bc98441d
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2016-04-09 22:54:00 +0800

    增加基本的mysql数据结构

--- LearningDjango/LearningDjango/settings.py
+++ LearningDjango/LearningDjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'OneApp',
 ]
 
 MIDDLEWARE_CLASSES = [
--- LearningDjango/OneApp/models.py
+++ LearningDjango/OneApp/models.py
@@ -3,3 +3,37 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    """
+    Description: Model Description
+    """
+    first_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    SHIRT_SIZES=(
+    	('s','Small'),
+    	('m','Medium'),
+    	('l','Large')
+    	)
+    shirt_size=models.CharField(max_length=1,choices=SHIRT_SIZES,default='s')
+    
+class Manufacturer(models.Model):
+	pass
+
+class Car(models.Model):
+	manufacturer=models.ForeignKey(Manufacturer,on_delete=models.CASCADE)
+
+class Reporter(models.Model):
+	name=models.CharField(max_length=120)
+	email=models.EmailField()
+	def __str__(self):
+		return '%s %s' %(self.name,self.email)
+
+class Article(models.Model):
+	headline=models.CharField(max_length=100)
+	pub_date=models.DateField()
+	reporter=models.ForeignKey(Reporter,on_delete=models.CASCADE)
+	def __str__(self):
+		return self.headline
+	class Meta:
+		ordering=('headline',)
