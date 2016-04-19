5ccaa498b8088cbf5a95d1e2c5214ca21e977b73
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2016-04-19 23:17:02 +0800

    增加模板继承 引入静态文件 js依赖

--- django_mytweets/django_mytweets/urls.py
+++ django_mytweets/django_mytweets/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from tweets.views import Index
+from tweets.views import Index,Profile
 
 urlpatterns = [
     url(r'^$',Index.as_view(),name='index'),
     url(r'^admin/', admin.site.urls),
+    url(r'^user/(\w+)/$',Profile.as_view()),
 ]
--- django_mytweets/tweets/views.py
+++ django_mytweets/tweets/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.views.generic import View
+from .models import Tweet
+from user_profile.models import User
+
 
 # def index(request):
 # 	if request.method == 'GET':
@@ -19,3 +22,12 @@ class Index(View):
 		return render(request,'base.html',params)
 	def post(self,request):
 		return HttpResponse('I am called from a post request')
+
+class Profile(View):
+	def get(self,request,username):
+		params=dict()
+		user=User.objects.get(username=username)
+		tweets=Tweet.objects.filter(user=user)
+		params['tweets']=tweets
+		params['user']=user
+		return render(request,'profile.html',params)
